e4d3f93c6c78083f1a2c03144933cb7946787716
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-01-06 21:23:10 +0200

    description fix

--- src/students/management/commands/generate_students.py
+++ src/students/management/commands/generate_students.py
@@ -4,18 +4,15 @@ from students.models import Student
 
 
 class Command(BaseCommand):
-    help = 'Generate random students'
+    help = 'Creates 100 fake students'
 
     def add_arguments(self, parser):
         parser.add_argument(
             '--number',
-            help='Delete poll instead of closing it',
+            help='Creates 100 fake students',
         )
 
     def handle(self, *args, **options):
         number = int(options.get('number') or 100)
         for _ in range(number):
             Student.generate_student()
-
-
-
