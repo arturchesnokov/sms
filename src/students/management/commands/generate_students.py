666f7ac2f0b8637d62e6feed69e144d9d29049c7
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-01-19 22:59:49 +0200

    generator fix

--- src/students/management/commands/generate_students.py
+++ src/students/management/commands/generate_students.py
@@ -3,6 +3,7 @@ import random
 from django.core.management.base import BaseCommand
 
 from students.models import Student, Group
+from teachers.models import Teacher
 
 
 class Command(BaseCommand):
@@ -11,12 +12,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         Group.objects.all().delete()
         Student.objects.all().delete()
+        Teacher.objects.all().delete()
+
         groups = []
+        teachers = []
+        students = []
 
         for _ in range(10):
+            teachers.append(Teacher.generate_teacher())
             groups.append(Group.generate_group())
 
         for _ in range(100):
             student = Student.generate_student()
             student.group = random.choice(groups)
+            students.append(student)
             student.save()
+
+        for group in groups:
+            group.curator = random.choice(teachers)
+            group.praepostor = random.choice(students)
+            group.save()
