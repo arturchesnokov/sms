2f1dac0d973888ec63161d53bb7e51923f166a0f
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-01-29 00:11:10 +0200

    part 2

--- src/students/signals.py
+++ src/students/signals.py
@@ -3,13 +3,9 @@ from django.dispatch import receiver
 from students.models import Student
 
 
-def only_numbers(st):
-    return ''.join([n for n in st if n.isdigit()])
-
-
 @receiver(pre_save, sender=Student)
 def pre_save_student(sender, instance, **kwargs):
     instance.email = instance.email.lower()
     instance.first_name = instance.first_name.strip().lower().capitalize()
     instance.last_name = instance.last_name.strip().lower().capitalize()
-    instance.telephone = only_numbers(instance.telephone)
+    instance.telephone = ''.join([n for n in instance.telephone if n.isdigit()])
--- src/teachers/signals.py
+++ src/teachers/signals.py
@@ -3,13 +3,9 @@ from django.dispatch import receiver
 from teachers.models import Teacher
 
 
-def only_numbers(st):
-    return ''.join([n for n in st if n.isdigit()])
-
-
 @receiver(pre_save, sender=Teacher)
 def pre_save_teacher(sender, instance, **kwargs):
     instance.email = instance.email.lower()
     instance.first_name = instance.first_name.strip().lower().capitalize()
     instance.last_name = instance.last_name.strip().lower().capitalize()
-    instance.telephone = only_numbers(instance.telephone)
+    instance.telephone = ''.join([n for n in instance.telephone if n.isdigit()])
