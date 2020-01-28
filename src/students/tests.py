d4c424801b852609ecf2878f269b3b4fa5d10677
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-01-29 01:09:46 +0200

    tests

--- src/students/tests.py
+++ src/students/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+import unittest
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class StudentListTest(TestCase):
+
+    def test_response_code(self):
+        client = Client()
+        response = client.get(reverse('students'))
+        self.assertEqual(response.status_code, 200)
