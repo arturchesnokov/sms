07bb6cf5ccaa98c0347f208d8c3ff1f061ffb8ee
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-02-08 00:59:28 +0200

    Beat added

--- src/sms/celery.py
+++ src/sms/celery.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sms.settings')
@@ -13,7 +14,29 @@ app = Celery('sms')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+# Celery application definition
+# http://docs.celeryproject.org/en/v4.0.2/userguide/configuration.html
+# CELERY_BROKER_URL = 'amqp://guest:**@localhost:5672'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+app.conf.accept_content = ['application/json']
+app.conf.result_serializer = 'json'
+app.conf.task_serializer = 'json'
+app.conf.timezone = 'UTC'
+
+app.conf.beat_schedule = {
+    'cleaning-old-data': {
+        'task': 'students.tasks.clean_old_data',
+        'schedule': crontab(),
+        'args': (7,)
+    }
+}
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
--- src/students/tasks.py
+++ src/students/tasks.py
@@ -1,4 +1,4 @@
-from celery import shared_task
+from celery import shared_task, task
 from time import sleep
 
 from django.core.mail import send_mail
@@ -7,3 +7,8 @@ from django.core.mail import send_mail
 @shared_task
 def send_email_async(subject, message, email_from, recipient_list):
     send_mail(subject, message, email_from, recipient_list)
+
+
+@task
+def clean_old_data(days):
+    print(f'Here must be method to clean data for {days} days.')
